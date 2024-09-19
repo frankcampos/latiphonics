1b35081c18adf38c76adfe94d7a7dc2ef4779283
frank campos <frankcampos85@gmail.com>
2024-09-18 22:16:26 -0500

    getting close

--- latiphonicsapi/models/symbol.py
+++ latiphonicsapi/models/symbol.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Symbol(models.Model):
-  picture_url = models.CharField(max_length=100)
-  pronunciation = models.CharField(max_length=100, default='add a sound')
+  picture_url = models.CharField(max_length=250)
+  pronunciation = models.CharField(max_length=250, default='add a sound')
   is_voiced = models.BooleanField(default=True)
   is_vowel = models.BooleanField(default=True)
-  sound_url = models.CharField(max_length=100, default='no sound added yet')
+  sound_url = models.CharField(max_length=250, default='no sound added yet')
