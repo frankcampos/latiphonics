00d760932f023f1102385e43463115b12fdee2db
frank campos <frankcampos85@gmail.com>
2024-09-18 22:43:48 -0500

    chore: Increase max length of video_url field in LearningSymbol model

--- latiphonicsapi/models/learning_symbol.py
+++ latiphonicsapi/models/learning_symbol.py
@@ -5,7 +5,7 @@ from .user import User
 class LearningSymbol(models.Model):
   prompt = models.TextField(max_length=200, default='add a prompt')
   example_phrases = models.TextField(max_length=200, default='add a phrase')
-  video_url = models.CharField(max_length=100, default='add a video')
+  video_url = models.CharField(max_length=1000, default='add a video')
   user= models.ForeignKey(User, on_delete=models.CASCADE, null=True)
   created_at = models.DateTimeField(default=timezone.now)
   updated_at = models.DateTimeField(auto_now=True, null=True)
