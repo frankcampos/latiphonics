787cbc54ef640e9fa91c2d8de9a0cbb81a9193e4
frank campos <frankcampos85@gmail.com>
2024-09-19 23:41:43 -0500

    Increase max length of video_url, picture_url, and sound_url fields in Symbol and LearningSymbol models

--- latiphonicsapi/models/symbol.py
+++ latiphonicsapi/models/symbol.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Symbol(models.Model):
-  picture_url = models.CharField(max_length=57730500)
-  pronunciation = models.CharField(max_length=88855500, default='add a sound')
+  picture_url = models.CharField(max_length=10000000)
+  pronunciation = models.CharField(max_length=10000000, default='add a sound')
   is_voiced = models.BooleanField(default=True)
   is_vowel = models.BooleanField(default=True)
-  sound_url = models.CharField(max_length=3000000, default='no sound added yet')
+  sound_url = models.CharField(max_length=10000000, default='no sound added yet')
